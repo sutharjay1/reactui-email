44145e5118ca876369384eb0189907408028d844
sutharjay1 <sutharjay3635@gmail.com>
2025-01-13 01:35:42 +0530

    test(app): Testing the file path

--- src/components/read-brand-source.ts
+++ src/components/read-brand-source.ts
@@ -2,7 +2,17 @@ import { promises as fs } from "fs";
 import path from "path";
 
 export async function readBrandSource(brand: string, componentName: string) {
-  const filePath = path.join(process.cwd(), "var","task","src", "email", `${brand}`, `${componentName}.tsx`);
+  try {
+    const files = await fs.readdir(process.cwd());
+    files.forEach((file) => {
+      console.log(file);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  const filePath = path.join(process.cwd(), "src", "email", `${brand}`, `${componentName}.tsx`);
+
   try {
     const source = await fs.readFile(filePath, "utf8");
     return source;
