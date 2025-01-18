7a4cc2bae5acb2fea837b53714c11d1066be46ca
sutharjay1 <sutharjay3635@gmail.com>
2025-01-18 18:52:55 +0530

    feat(app): Added react.mail cli command

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,4 +1,5 @@
 import { t } from "@/features/lib/utils";
+import CliCommands from "./cli-commands";
 
 interface PageHeaderProps {
   title: string;
@@ -7,11 +8,24 @@ interface PageHeaderProps {
 
 export default function PageHeader({ title, children }: PageHeaderProps) {
   return (
-    <div className="mb-16 text-left">
-      <h1 className="mb-3 text-3xl font-extrabold capitalize tracking-tight text-foreground md:text-4xl">
-        {t(title)}
-      </h1>
-      <p className="text-lg text-muted-foreground">{children}</p>
-    </div>
+    <>
+      <div className="mb-12 text-left">
+        <h1 className="mb-3 text-3xl font-extrabold capitalize tracking-tight text-foreground md:text-4xl">
+          {t(title)}
+        </h1>
+        <p className="text-lg text-muted-foreground">{children}</p>
+      </div>
+
+      <div className="mb-12">
+        <h1 className="mb-3 text-2xl font-extrabold capitalize tracking-tight text-foreground md:text-3xl">
+          Install
+        </h1>
+        <p className="mb-4 text-base text-muted-foreground">
+          Install component from your command line.
+        </p>
+
+        <CliCommands />
+      </div>
+    </>
   );
 }
