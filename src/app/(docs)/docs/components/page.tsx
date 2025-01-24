b78116c24a8338ecb566c858c8e6ae50a4d0e20e
sutharjay1 <sutharjay3635@gmail.com>
2025-01-24 10:29:19 +0530

    fix(app): Component Fix

--- src/app/(docs)/docs/components/page.tsx
+++ src/app/(docs)/docs/components/page.tsx
@@ -1,9 +1,5 @@
-import React from "react";
-
-type Props = React.HTMLAttributes<HTMLDivElement>;
-
-const Component: React.FC<Props> = (props) => {
-  return <div {...props}>Component</div>;
+const Component = () => {
+  return <div>Component</div>;
 };
 
 export default Component;
